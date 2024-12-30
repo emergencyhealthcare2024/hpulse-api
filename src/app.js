de718bd30c530a9f0dc2b64dc04f58b608699c3a
emergencyhealthcare2024 <emergencyhealthcare2024@gmail.com>
2024-12-30 12:39:02 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,8 +8,8 @@ dotenv.config();
 
 // Initialize Razorpay instance
 const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
+  key_id: process.env.RAZORPAY_KEY_TEST_ID,
+  key_secret: process.env.RAZORPAY_KEY_TEST_SECRET,
 });
 
 const app = express();
